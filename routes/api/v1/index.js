c38238a16b7bb26fe37375139591cfa4877dee7c
nickbevers <nick.bevers9@gmail.com>
2021-04-15 14:53:58 +0200

    feat: Add basic functionality with link to controller

--- routes/api/v1/index.js
+++ routes/api/v1/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+const controller = require("../../../controllers/api/v1/transactions");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Make new transaction
+router.post("/transfers", controller.newTransaction)
+
+//Get all transactions from user
+router.get("/transfers", controller.getTransactions)
+
+//Get all details from specifc transaction
+router.get("/transfers/:id", controller.getTransferById)
+
+//Get #coins per user -> make leaderboard out of it
+router.get("/leaderboard", controller.getLeaderboard)
 
 module.exports = router;
